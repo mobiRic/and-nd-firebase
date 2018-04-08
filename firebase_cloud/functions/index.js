08aa819c8e562d306db013c40d445a1d828c635c
Richard Le Mesurier <richard@glowworm.mobi>
2018-04-08 20:06:40 +0200

    Fix incorrect param order

--- firebase_cloud/functions/index.js
+++ firebase_cloud/functions/index.js
@@ -6,3 +6,41 @@ const functions = require('firebase-functions');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+/**
+ * Replaces keywords with emoji in the "text" key of messages
+ * pushed to /messages
+ *
+ * @type {CloudFunction<Change<DataSnapshot>>}
+ */
+exports.emojify =
+  functions.database
+    .ref('/messages/{pushId}/text')
+    .onWrite(event => {
+
+      if (!event.data.val()) {
+        console.log("delete event found - exiting");
+        return null;
+      }
+
+      if (event.data.previous.val()) {
+        console.log("rewrite event found - exiting");
+        return null;
+      }
+
+      const original = event.data.val();
+      const emojiText = emojifyText(original);
+
+      return event.data.ref.set(emojiText);
+    });
+
+/**
+ * Replaces certain text strings with emoji icons.
+ * @param text message entered by the user
+ * @return message with embedded emoji icons
+ */
+function emojifyText(text) {
+  return text
+    .replace(/\blol\b/ig, "😂")
+    .replace(/\bcat\b/ig, "😸");
+}
